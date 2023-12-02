603fcf452c866c7eacbc010e0f93d91751583814
Meseret <mesiteklu2018@gmail.com>
2023-12-01 22:35:24 -0700

    modified codes

--- @latest/src/Avatar.jsx
+++ @latest/src/Avatar.jsx
@@ -3,8 +3,8 @@ import React from "react"
 function Avatar() {
   return
 <img className="circle-img" src="{props.img}" alt="avatar_img" />
-
 }
+
 
 
 export default Avatar; 
--- @latest/src/Card.jsx
+++ @latest/src/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import ReactDom from "reactdom"
 
 function Card(props) {
   return (
